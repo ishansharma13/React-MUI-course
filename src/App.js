b191536a2fe9e754b176790e9a83442fabf8011c
Ishan <ishan@mtlbigger.com>
2021-12-12 21:08:15 +0530

    Used ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/ui/Header';
+import theme from './components/ui/Theme';
+// import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 function App() {
   return (
-    <div className="App">
+   
+        <ThemeProvider theme={theme} >
         <Header>
         My Application
         </Header>
         Hello
-    </div>
+        </ThemeProvider>
   );
 }
 
