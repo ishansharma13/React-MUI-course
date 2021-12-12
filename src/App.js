bb503fe6b88a88548c619abbfd481182e5d7d92f
Ishan <ishan@mtlbigger.com>
2021-12-12 20:07:44 +0530

    Elevated App Bar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
         <Header>
-          My Application
+        My Application
         </Header>
-      HELLO
+        Hello
     </div>
   );
 }
