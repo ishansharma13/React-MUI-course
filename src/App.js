e12749af3ebdf1b47dd8e5695fe3ab6f7777eeb8
Ishan <ishan@mtlbigger.com>
2021-12-13 16:34:12 +0530

    Section-2 Complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/ui/Header';
 import theme from './components/ui/Theme';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 // import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
@@ -8,10 +9,19 @@ function App() {
   return (
    
         <ThemeProvider theme={theme} >
-        <Header>
-        My Application
-        </Header>
-        Hello
+        <BrowserRouter >
+        <Header />
+        
+        <Switch>
+        <Route exact path="/" component={()=><div>Home</div>} />
+        <Route exact path="/services" component={()=><div>Services</div>} />
+        <Route exact path="/revolution" component={()=><div>The Revolution</div>} />
+        <Route exact path="/about-us" component={()=><div>About Us</div>} />
+        <Route exact path="/contact-us" component={()=><div>Contact Us</div>} />
+        <Route exact path="/estimate" component={()=><div>Estimate</div>} />
+        
+        </Switch>
+        </BrowserRouter>
         </ThemeProvider>
   );
 }
